37aa730af9362a8a94d576f73d5062348bf2e80a
pranjal shukla <138642719+Pranjal7879@users.noreply.github.com>
2025-05-07 13:29:30 +0530

    fifth push

--- beeta/pagination.py
+++ beeta/pagination.py
@@ -3,4 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 class CustomProductPagination(PageNumberPagination):
     page_size = 5
     page_size_query_param = 'page_size'
-    max_page_size = 40
+    max_page_size = 50
